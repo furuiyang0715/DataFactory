f045af26b72985feeac0c4486b8628ff500ce79f
furuiyang <furuiyang0715@gmail.com>
2020-03-22 23:34:01 +0800

    dix

--- hkland_elistocks/README.md
+++ hkland_elistocks/README.md
@@ -2,4 +2,4 @@ docker build -t registry.cn-shenzhen.aliyuncs.com/jzdev/dcfactory/kland_scelisto
 docker push registry.cn-shenzhen.aliyuncs.com/jzdev/dcfactory/kland_scelistocks:v0.0.1
 
 sudo docker pull registry.cn-shenzhen.aliyuncs.com/jzdev/dcfactory/kland_scelistocks:v0.0.1
-sudo docker run -itd --name scelistocks registry.cn-shenzhen.aliyuncs.com/jzdev/dcfactory/kland_scelistocks:v0.0.1
+sudo docker run -itd --name scelistocks --env LOCAL=0 registry.cn-shenzhen.aliyuncs.com/jzdev/dcfactory/kland_scelistocks:v0.0.1
--- hkland_elistocks/main.py
+++ hkland_elistocks/main.py
@@ -1,17 +1,26 @@
 import sys
 sys.path.append("./../")
 
+from hkland_elistocks.single_process import fix
 from hkland_elistocks.configs import LOCAL
 from hkland_elistocks.sh_human_gene import SHHumanTools
 from hkland_elistocks.zh_human_gene import ZHHumanTools
 
-sh = SHHumanTools()
-if LOCAL:
-    sh.create_target_table()
-sh._process()
+
+def main():
+    sh = SHHumanTools()
+    if LOCAL:
+        sh.create_target_table()
+    sh._process()
 
 
-zh = ZHHumanTools()
-if LOCAL:
-    zh.create_target_table()
-zh._process()
+    zh = ZHHumanTools()
+    if LOCAL:
+        zh.create_target_table()
+    zh._process()
+
+
+    fix()
+
+
+main()
