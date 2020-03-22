1dd0411854c8425bc881f654190eb4e828cf94a2
furuiyang <furuiyang0715@gmail.com>
2020-03-22 16:01:51 +0800

    rename

--- hkland_elistocks/main.py
+++ hkland_elistocks/main.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("./../")
 
 from hkland_elistocks.configs import LOCAL
-from hkland_elistocks.human_gene import HumanTools
+from hkland_elistocks.sh_human_gene import HumanTools
 from hkland_elistocks.zh_human_gene import ZHHumanTools
 
 sh = HumanTools()
