b42758dd9b081d4aa4e1b7bac9885cfaf116edef
furuiyang <furuiyang0715@gmail.com>
2020-04-03 18:05:06 +0800

    同花顺获取数据

--- hkland_flow/jqka10_flow.py
+++ hkland_flow/jqka10_flow.py
@@ -1,4 +1,7 @@
+import re
+
 import execjs
+import requests
 
 
 class SFLgthisdataspiderSpider(object):
@@ -13,19 +16,44 @@ class SFLgthisdataspiderSpider(object):
             'X-Requested-With': 'XMLHttpRequest',
             'Connection': 'keep-alive',
         }
+        self.base_url = 'http://data.10jqka.com.cn/hgt/{}/'
+        self.category_lst = ['hgtb',   # '沪股通'
+                             'ggtb',   # '港股通(沪)'
+                             'sgtb',   # '深股通'
+                             'ggtbs',  # '港股通(深)'
+                             ]
 
     @property
-    def cookie(self):
+    def cookies(self):
         """由于同花顺生成cookie的文件是不变的, 所以直接用文件保存方便获取"""
         with open('jqka.js', 'r') as f:
             jscont = f.read()
         cont = execjs.compile(jscont)
         cookie_v = cont.call('v')
-        return cookie_v
+        cookies = {
+            'v': cookie_v,
+        }
+        return cookies
+
+    def get(self, url):
+        resp = requests.get(url, headers=self.headers, cookies=self.cookies)
+        if resp.status_code == 200:
+            return resp.text
+
+    def _start(self):
+        for category in self.category_lst:
+            url = self.base_url.format(category)
+            page = self.get(url)
+            ret = re.findall(r"var dataDay = (.*);", page)
+            if ret:
+                datas = eval(ret[0])[0]
+                print(datas)
 
 
 if __name__ == "__main__":
     sf = SFLgthisdataspiderSpider()
-    print(sf.cookie)
+    # print(sf.cookies)
+    sf._start()
+
 
     pass
