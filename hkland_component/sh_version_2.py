a371c22cb5d9f342c48c0d4f24b2be37b1e48c9f
furuiyang <furuiyang0715@gmail.com>
2020-04-20 13:31:55 +0800

    test

---
+++ hkland_component/sh_version_2.py
@@ -0,0 +1,308 @@
+import datetime
+
+from hkland_component.configs import (JUY_HOST, JUY_PORT, JUY_USER, JUY_PASSWD, JUY_DB, DATACENTER_HOST,
+                                      DATACENTER_PORT, DATACENTER_USER, DATACENTER_PASSWD, DATACENTER_DB, SPIDER_HOST,
+                                      SPIDER_PORT, SPIDER_USER, SPIDER_PASSWD, SPIDER_DB, TARGET_HOST, TARGET_PORT,
+                                      TARGET_USER, TARGET_PASSWD, TARGET_DB)
+from hkland_component.my_log import logger
+from hkland_component.sql_pool import PyMysqlPoolBase
+
+
+class ZHSCComponent(object):
+    juyuan_cfg = {
+        "host": JUY_HOST,
+        "port": JUY_PORT,
+        "user": JUY_USER,
+        "password": JUY_PASSWD,
+        "db": JUY_DB,
+    }
+
+    dc_cfg = {
+        "host": DATACENTER_HOST,
+        "port": DATACENTER_PORT,
+        "user": DATACENTER_USER,
+        "password": DATACENTER_PASSWD,
+        "db": DATACENTER_DB,
+    }
+
+    source_cfg = {
+        "host": SPIDER_HOST,
+        "port": SPIDER_PORT,
+        "user": SPIDER_USER,
+        "password": SPIDER_PASSWD,
+        "db": SPIDER_DB,
+    }
+
+    target_cfg = {
+        "host": TARGET_HOST,
+        "port": TARGET_PORT,
+        "user": TARGET_USER,
+        "password": TARGET_PASSWD,
+        "db": TARGET_DB,
+    }
+
+    def init_sql_pool(self, sql_cfg: dict):
+        pool = PyMysqlPoolBase(**sql_cfg)
+        return pool
+
+    def __init__(self):
+        #  ID  | CompType | InnerCode | SecuCode | InDate | OutDate | Flag | CREATETIMEJZ | UPDATETIMEJZ | CMFID | CMFTime |
+        #  ID  | CompType | InnerCode | SecuCode | InDate | OutDate | Flag | UpdateTime   | JSID
+        self.juyuan_table_name = 'lc_zhsccomponent'
+        self.dc_table_name = 'hkland_sgcomponent'
+        self.target_table_name = 'hkland_sgcomponent'
+        self.fields = ["CompType", "InnerCode", "SecuCode", "InDate", "OutDate", "Flag"]
+        self.zh_change_table_name = 'hkex_lgt_change_of_szse_securities_lists'
+        self.zh_list_table_name = 'hkex_lgt_szse_securities'
+        self.hk_change_table_name = 'lgt_szse_underlying_securities_adjustment'
+        self.hk_list_table_name = 'hkex_lgt_szse_list_of_eligible_securities'
+
+        self.stats_todonothing = [
+            'Addition to List of Eligible SZSE Securities for Margin Trading and List of Eligible SZSE Securities for Short Selling',
+            'Remove from List of Eligible SZSE Securities for Margin Trading and List of Eligible SZSE Securities for Short Selling',
+            'SZSE Stock Code and Stock Name are changed from 22 and SHENZHEN CHIWAN WHARF HOLDINGS respectively',
+            'SZSE Stock Code and Stock Name are changed to 1872 and CHINA MERCHANTS PORT GROUP respectively',
+            'SZSE Stock Code and Stock Name are changed to 001914 and CHINA MERCHANTS PPTY OPERATION&SERVICE respectively',
+            'SZSE Stock Code and Stock Name are changed from 000043 and AVIC SUNDA HOLDING respectively',
+            'Buy orders suspended',
+            'Buy orders resumed',
+        ]
+        self.stats_addition = ['Addition']
+        self.stats_recover = [
+            'Addition (from List of Special SZSE Securities/Special China Connect Securities (stocks eligible for sell only))',
+        ]
+        self.stats_removal = ['Removal']
+        self.stats_transfer = [
+            'Transfer to List of Special SZSE Securities/Special China Connect Securities (stocks eligible for sell only)'
+        ]
+
+    def juyuan_stats(self):
+        fields_str = ",".join(self.fields)
+        juyuan = self.init_sql_pool(self.juyuan_cfg)
+        sql = 'select {} from {}; '.format(fields_str, self.juyuan_table_name)
+        datas = juyuan.select_all(sql)
+        juyuan.dispose()
+        return datas
+
+    def dc_stats(self):
+        fields_str = ",".join(self.fields)
+        dc = self.init_sql_pool(self.dc_cfg)
+        sql = 'select {} from {}; '.format(fields_str, self.dc_table_name)
+        datas = dc.select_all(sql)
+        dc.dispose()
+        return datas
+
+    def get_zh_changes(self):
+        # 获取深股通的更改
+        spider_source = self.init_sql_pool(self.source_cfg)
+        sql = 'select * from {} where Time = (select max(Time) from {});'.format(
+            self.zh_change_table_name, self.zh_change_table_name)
+        zh_changes = spider_source.select_all(sql)
+        spider_source.dispose()
+        return zh_changes
+
+    def get_hk_changes(self):
+        # 获取深港通中港的更改
+        spider_source = self.init_sql_pool(self.source_cfg)
+        sql = 'select * from {} where Time = (select max(Time) from {}); '.format(
+            self.hk_change_table_name, self.hk_change_table_name)
+        hk_changes = spider_source.select_all(sql)
+        spider_source.dispose()
+        return hk_changes
+
+    def check_target_exist(self, record):
+        target = self.init_sql_pool(self.target_cfg)
+        if record.get("InDate"):
+            is_exist = target.select_one(
+                "select * from {} where CompType = {} and SecuCode = '{}' and InDate = '{}'; ".format(
+                    self.target_table_name, record.get("CompType"), record.get("SecuCode"), record.get("InDate")
+                ))
+        else:
+            is_exist = target.select_one(
+                "select * from {} where CompType = {} and SecuCode = '{}' and OutDate = '{}';".format(
+                    self.target_table_name, record.get("CompType"), record.get("SecuCode"), record.get("OutDate")
+                ))
+        target.dispose()
+        if is_exist:
+            return True
+        else:
+            return False
+
+    def is_in_list(self, code):
+        target = self.init_sql_pool(self.target_cfg)
+        ret = target.select_all(
+            "select flag from {} where SecuCode = '{}' and InDate = (select max(InDate) from {} where SecuCode = '{}'); ".format(
+                self.target_table_name, code, self.target_table_name, code))[0]
+        if ret.get("flag") == 1:
+            return True
+        else:
+            return False
+
+    def check_hk_list(self):
+        def get_spider_hk_list():
+            spider_source = self.init_sql_pool(self.source_cfg)
+            sql = 'select distinct(SecuCode) from {} where Time = (select max(Time) from {});'.format(
+                self.hk_list_table_name, self.hk_list_table_name)
+            ret = spider_source.select_all(sql)
+            spider_source.dispose()
+            hk_list = [r.get("SecuCode") for r in ret]
+            return hk_list
+
+        def get_target_hk_list():
+            target = self.init_sql_pool(self.target_cfg)
+            sql = 'select SecuCode from {} where CompType = 4 and Flag = 1;'.format(self.target_table_name)
+            ret = target.select_all(sql)
+            target.dispose()
+            hk_list = [r.get("SecuCode") for r in ret]
+            return hk_list
+
+        spider_hk_list = set(get_spider_hk_list())
+        target_hk_list = set(get_target_hk_list())
+        logger.info(spider_hk_list == target_hk_list)
+        print(spider_hk_list - target_hk_list)
+        print(target_hk_list - spider_hk_list)
+        return spider_hk_list == target_hk_list
+
+    def check_zh_list(self):
+        def get_spider_zh_list():
+            spider_source = self.init_sql_pool(self.source_cfg)
+            sql = 'select distinct(SSESCode) from {} where Date = (select max(Date) from {});'.format(
+                self.zh_list_table_name, self.zh_list_table_name)
+            ret = spider_source.select_all(sql)
+            spider_source.dispose()
+            zh_list = [r.get("SSESCode") for r in ret]
+            return zh_list
+
+        def get_target_zh_list():
+            target = self.init_sql_pool(self.target_cfg)
+            sql = 'select SecuCode from {} where CompType = 3 and Flag = 1;'.format(self.target_table_name)
+            ret = target.select_all(sql)
+            target.dispose()
+            zh_list = [r.get("SecuCode") for r in ret]
+            return zh_list
+
+        spider_zh_list = set(get_spider_zh_list())
+        target_zh_list = set(get_target_zh_list())
+        logger.info(spider_zh_list == target_zh_list)
+        return spider_zh_list == target_zh_list
+
+    def process_zh_changes(self, zh_changes):
+        def get_zh_inner_code(secu_code):
+            juyuan = self.init_sql_pool(self.juyuan_cfg)
+            sql = 'select * from secumain where SecuMarket = 90 and SecuCode = "{}";'.format(secu_code)
+            ret = juyuan.select_all(sql)
+            juyuan.dispose()
+            if len(ret) != 1:
+                print("请检查: {} {}".format(secu_code, len(ret)))  # 请检查: 000043 0; 000022 0
+            else:
+                inner_code = ret[0].get("InnerCode")
+                return inner_code
+
+        for change in zh_changes:
+            # print(">>> ", change)
+            stats = change.get("Ch_ange")
+            if stats in self.stats_todonothing:
+                continue
+
+            secu_code = change.get("SSESCode")
+            effective_date = datetime.datetime.combine(change.get('EffectiveDate'), datetime.datetime.min.time())
+
+            # 加入成分股的
+            if stats in self.stats_addition:
+                record = {"CompType": 3, "SecuCode": secu_code, "InDate": effective_date}
+                is_exist = self.check_target_exist(record)
+                if not is_exist:
+                    inner_code = get_zh_inner_code(secu_code)
+                    update_time = change.get("Time")
+                    record.update({"InnerCode": inner_code, "Flag": 1})
+                    logger.info("新增一条记录: {}".format(record))
+
+            elif stats in self.stats_recover:
+                record = {"CompType": 3, "SecuCode": secu_code, "InDate": effective_date}
+                is_exist = self.check_target_exist(record)
+                if not is_exist:
+                    logger.info("新增一条恢复记录{}".format(record))
+
+            elif stats in self.stats_transfer:
+                record = {"CompType": 3, "SecuCode": secu_code, "OutDate": effective_date}
+                is_exist = self.check_target_exist(record)
+                if not is_exist:
+                    logger.info("新增一条移除记录{}".format(record))
+
+            elif stats in self.stats_removal:
+                record = {"CompType": 3, "SecuCode": secu_code, "OutDate": effective_date}
+                is_exist = self.check_target_exist(record)
+                if not is_exist:
+                    is_in_list = self.is_in_list(secu_code)
+                    if is_in_list:
+                        logger.info("需要剔除记录 {}".format(record))
+                    else:
+                        # logger.info("此条剔除记录已被记录{}".format(record))
+                        pass
+            else:
+                logger.warning("其他状态: {}".format(stats))
+
+    def process_hk_changes(self, hk_changes):
+        def get_hk_inner_code(secu_code):
+            juyuan = self.init_sql_pool(self.juyuan_cfg)
+            sql = 'select * from hk_secumain where SecuCode = "{}";'.format(secu_code)
+            ret = juyuan.select_all(sql)
+            juyuan.dispose()
+            if len(ret) != 1:
+                print("请检查: {} {}".format(secu_code, len(ret)))
+            else:
+                inner_code = ret[0].get("InnerCode")
+                return inner_code
+
+        for change in hk_changes:
+            secu_code = change.get("SecuCode")
+            effective_date = datetime.datetime.combine(change.get('AdjustTime'), datetime.datetime.min.time())
+
+            stats = change.get("AdjustContent")
+            if stats == '调入':
+                record = {"CompType": 4, "SecuCode": secu_code, "InDate": effective_date}
+                is_exist = self.check_target_exist(record)
+                if not is_exist:
+                    inner_code = get_hk_inner_code(secu_code)
+                    update_time = change.get("Time")
+                    record.update({"InnerCode": inner_code, "Flag": 1})
+                    logger.info("需要新增一条调入记录 {}".format(record))
+            elif stats == '调出':
+                record = {"CompType": 4, "SecuCode": secu_code, "OutDate": effective_date}
+                is_exist = self.check_target_exist(record)
+                if not is_exist:
+                    logger.info("需要新增一条调出记录{}".format(record))
+
+    def start(self):
+        juyuan_datas = self.juyuan_stats()
+        dc_datas = self.dc_stats()
+
+        # for data in juyuan_datas:
+        #     assert data in dc_datas
+        # for data in dc_datas:
+        #     assert data in juyuan_datas
+
+        # zh_changes = self.get_zh_changes()
+        # self.process_zh_changes(zh_changes)
+        # self.check_zh_list()
+
+        # hk_changes = self.get_hk_changes()
+        # self.process_hk_changes(hk_changes)
+        self.check_hk_list()
+
+
+if __name__ == "__main__":
+    tool = ZHSCComponent()
+    tool.start()
+
+
+
+'''记录
+2020.04.20 
+新增一条恢复记录{'CompType': 3, 'SecuCode': '000043', 'InDate': datetime.datetime(2019, 12, 16, 0, 0)} 
+新增一条记录: {'CompType': 3, 'SecuCode': '000022', 'InDate': datetime.datetime(2018, 1, 2, 0, 0), 'InnerCode': None, 'Flag': 1}
+
+需要新增一条调入记录 {'CompType': 4, 'SecuCode': '00697', 'InDate': datetime.datetime(2020, 4, 15, 0, 0), 'InnerCode': 1000543, 'Flag': 1}  
+
+
+'''
--- hkland_component/single_insert.py
+++ hkland_component/single_insert.py
@@ -7,6 +7,13 @@
 请检查: 000022 0
 [I 2020-03-20 10:02:04|merge_lc_zhsccomponent|process_zh_changes|123] 新增一条记录: {'CompType': 3, 'SecuCode': '000022', 'InDate': datetime.datetime(2018, 1, 2, 0, 0), 'InnerCode': None, 'Flag': 1}
 '''
+import datetime
+import traceback
+
+from hkland_component.configs import TARGET_HOST, TARGET_PORT, TARGET_USER, TARGET_PASSWD, TARGET_DB
+from hkland_component.my_log import logger
+from hkland_component.sql_pool import PyMysqlPoolBase
+
 '''
 mysql>select SSESCode, EffectiveDate, Ch_ange from hkex_lgt_change_of_szse_securities_lists where SSESCode = '000043' order by  EffectiveDate;
 +----------+---------------+------------------------------------------------------------------------------------------------------------------+
@@ -69,3 +76,87 @@ mysql> select * from lc_zhsccomponent where SecuCode = '001872';
 '''
 
 
+''' 4.20 运行结果同上 
+[I 2020-04-20 10:36:48|sh_version_2|process_zh_changes|164] 新增一条恢复记录{'CompType': 3, 'SecuCode': '000043', 'InDate': datetime.datetime(2019, 12, 16, 0, 0)}
+请检查: 000022 0
+[I 2020-04-20 10:38:05|sh_version_2|process_zh_changes|158] 新增一条记录: {'CompType': 3, 'SecuCode': '000022', 'InDate': datetime.datetime(2018, 1, 2, 0, 0), 'InnerCode': None, 'Flag': 1}
+
+
+[I 2020-04-20 10:49:20|sh_version_2|process_hk_changes|244] 需要新增一条调入记录 {'CompType': 4, 'SecuCode': '00697', 'InDate': datetime.datetime(2020, 4, 15, 0, 0), 'InnerCode': 1000543, 'Flag': 1} 
+mysql> select * from hkland_sgcomponent where SecuCode = '00697';
++------+----------+-----------+----------+---------------------+---------------------+------+---------------------+---------------------+-------+---------+
+| ID   | CompType | InnerCode | SecuCode | InDate              | OutDate             | Flag | CREATETIMEJZ        | UPDATETIMEJZ        | CMFID | CMFTime |
++------+----------+-----------+----------+---------------------+---------------------+------+---------------------+---------------------+-------+---------+
+| 9920 |        4 |   1000543 | 00697    | 2020-03-09 00:00:00 | 2020-03-30 00:00:00 |    2 | 2020-03-31 06:10:26 | 2020-03-31 06:10:26 |  NULL | NULL    |
++------+----------+-----------+----------+---------------------+---------------------+------+---------------------+---------------------+-------+---------+
+1 row in set (0.02 sec)
+
+需要插入一条数据: {'CompType': 4, 'SecuCode': '00697', 'InDate': datetime.datetime(2020, 4, 15, 0, 0), 'InnerCode': 1000543, 'Flag': 1}  
+
+
+'''
+
+
+def contract_sql(to_insert: dict, table: str, update_fields: list):
+    ks = []
+    vs = []
+    for k in to_insert:
+        ks.append(k)
+        vs.append(to_insert.get(k))
+    fields_str = "(" + ",".join(ks) + ")"
+    values_str = "(" + "%s," * (len(vs) - 1) + "%s" + ")"
+    base_sql = '''INSERT INTO `{}` '''.format(table) + fields_str + ''' values ''' + values_str
+    on_update_sql = ''' ON DUPLICATE KEY UPDATE '''
+    update_vs = []
+    for update_field in update_fields:
+        on_update_sql += '{}=%s,'.format(update_field)
+        update_vs.append(to_insert.get(update_field))
+    on_update_sql = on_update_sql.rstrip(",")
+    sql = base_sql + on_update_sql + """;"""
+    vs.extend(update_vs)
+    return sql, tuple(vs)
+
+
+def save(to_insert, table: str, update_fields: list):
+
+    target_cfg = {
+        "host": TARGET_HOST,
+        "port": TARGET_PORT,
+        "user": TARGET_USER,
+        "password": TARGET_PASSWD,
+        "db": TARGET_DB,
+    }
+
+    def _init_pool(cfg: dict):
+        pool = PyMysqlPoolBase(**cfg)
+        return pool
+
+    product = _init_pool(target_cfg)
+
+    try:
+        insert_sql, values = contract_sql(to_insert, table, update_fields)
+        count = product.insert(insert_sql, values)
+    except:
+        traceback.print_exc()
+        logger.warning("失败")
+        count = None
+    else:
+        if count:
+            logger.info("更入新数据 {}".format(to_insert))
+    finally:
+        product.dispose()
+    return count
+
+
+def human_insert(table: str, data: dict):
+    """
+    将不一致的数据单独插入
+    :param table:
+    :param data:
+    :return:
+    """
+    fields = ["CompType", "InnerCode", "SecuCode", "InDate", "OutDate", "Flag"]
+    save(data, table, fields)
+
+
+ret = human_insert("hkland_sgcomponent", {'CompType': 4, 'SecuCode': '00697', 'InDate': datetime.datetime(2020, 4, 15, 0, 0), 'InnerCode': 1000543, 'Flag': 1})
