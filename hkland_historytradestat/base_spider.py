a9be5f016cd54ef6374a9e7f2ed609e5fdfcfc22
furuiyang <furuiyang0715@gmail.com>
2020-06-10 17:07:22 +0800

    更新 base 以及 pool

--- hkland_historytradestat/base_spider.py
+++ hkland_historytradestat/base_spider.py
@@ -1,9 +1,17 @@
+import base64
+import hashlib
+import hmac
+import json
 import logging
+import time
+import traceback
+import urllib.parse
 
-from hkland_historytradestat.configs import (SPIDER_MYSQL_USER, SPIDER_MYSQL_PORT, SPIDER_MYSQL_PASSWORD,
-                                             SPIDER_MYSQL_HOST, SPIDER_MYSQL_DB, PRODUCT_MYSQL_HOST, PRODUCT_MYSQL_PORT,
-                                             PRODUCT_MYSQL_USER, PRODUCT_MYSQL_PASSWORD, PRODUCT_MYSQL_DB, JUY_HOST,
-                                             JUY_PORT, JUY_USER, JUY_PASSWD, JUY_DB)
+import requests
+
+from hkland_historytradestat.configs import (PRODUCT_MYSQL_HOST, PRODUCT_MYSQL_PORT, PRODUCT_MYSQL_USER,
+                                             PRODUCT_MYSQL_PASSWORD, PRODUCT_MYSQL_DB, JUY_HOST, JUY_PORT,
+                                             JUY_USER, JUY_PASSWD, JUY_DB, SECRET, TOKEN)
 from hkland_historytradestat.sql_pool import PyMysqlPoolBase
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -11,15 +19,6 @@ logger = logging.getLogger(__name__)
 
 
 class BaseSpider(object):
-
-    spider_cfg = {
-        "host": SPIDER_MYSQL_HOST,
-        "port": SPIDER_MYSQL_PORT,
-        "user": SPIDER_MYSQL_USER,
-        "password": SPIDER_MYSQL_PASSWORD,
-        "db": SPIDER_MYSQL_DB,
-    }
-
     product_cfg = {
         "host": PRODUCT_MYSQL_HOST,
         "port": PRODUCT_MYSQL_PORT,
@@ -35,6 +34,11 @@ class BaseSpider(object):
         "password": JUY_PASSWD,
         "db": JUY_DB,
     }
+
+    def __init__(self):
+        self.tool_table_name = 'base_table_updatetime'
+        self.juyuan_client = None
+        self.product_client = None
 
     def _init_pool(self, cfg: dict):
         """
@@ -52,7 +56,25 @@ class BaseSpider(object):
         pool = PyMysqlPoolBase(**cfg)
         return pool
 
-    def contract_sql(self, to_insert: dict, table: str, update_fields: list):
+    def _juyuan_init(self):
+        if not self.juyuan_client:
+            self.juyuan_client = self._init_pool(self.juyuan_cfg)
+
+    def _product_init(self):
+        if not self.product_client:
+            self.product_client = self._init_pool(self.product_cfg)
+
+    def __del__(self):
+        if self.juyuan_client:
+            self.juyuan_client.dispose()
+        if self.product_client:
+            self.product_client.dispose()
+
+    def contract_sql(self, datas, table: str, update_fields: list):
+        if not isinstance(datas, list):
+            datas = [datas, ]
+
+        to_insert = datas[0]
         ks = []
         vs = []
         for k in to_insert:
@@ -61,12 +83,88 @@ class BaseSpider(object):
         fields_str = "(" + ",".join(ks) + ")"
         values_str = "(" + "%s," * (len(vs) - 1) + "%s" + ")"
         base_sql = '''INSERT INTO `{}` '''.format(table) + fields_str + ''' values ''' + values_str
-        on_update_sql = ''' ON DUPLICATE KEY UPDATE '''
-        update_vs = []
-        for update_field in update_fields:
-            on_update_sql += '{}=%s,'.format(update_field)
-            update_vs.append(to_insert.get(update_field))
-        on_update_sql = on_update_sql.rstrip(",")
-        sql = base_sql + on_update_sql + """;"""
-        vs.extend(update_vs)
-        return sql, tuple(vs)
+
+        params = []
+        for data in datas:
+            vs = []
+            for k in ks:
+                vs.append(data.get(k))
+            params.append(vs)
+
+        if update_fields:
+            # https://stackoverflow.com/questions/12825232/python-execute-many-with-on-duplicate-key-update/12825529#12825529
+            # sql = 'insert into A (id, last_date, count) values(%s, %s, %s) on duplicate key update last_date=values(last_date),count=count+values(count)'
+            on_update_sql = ''' ON DUPLICATE KEY UPDATE '''
+            for update_field in update_fields:
+                on_update_sql += '{}=values({}),'.format(update_field, update_field)
+            on_update_sql = on_update_sql.rstrip(",")
+            sql = base_sql + on_update_sql + """;"""
+        else:
+            sql = base_sql + ";"
+        return sql, params
+
+    def _batch_save(self, sql_pool, to_inserts, table, update_fields):
+        try:
+            sql, values = self.contract_sql(to_inserts, table, update_fields)
+            count = sql_pool.insert_many(sql, values)
+        except:
+            traceback.print_exc()
+            logger.warning("失败")
+        else:
+            logger.info("批量插入的数量是{}".format(count))
+            sql_pool.end()
+            return count
+
+    def _save(self, sql_pool, to_insert, table, update_fields):
+        try:
+            insert_sql, values = self.contract_sql(to_insert, table, update_fields)
+            value = values[0]
+            count = sql_pool.insert(insert_sql, value)
+        except:
+            traceback.print_exc()
+            logger.warning("失败")
+        else:
+            if count == 1:
+                logger.info("插入新数据 {}".format(to_insert))
+            elif count == 2:
+                logger.info("刷新数据 {}".format(to_insert))
+            else:
+                logger.info("已有数据 {} ".format(to_insert))
+            sql_pool.end()
+            return count
+
+    def ding(self, msg):
+        def get_url():
+            timestamp = str(round(time.time() * 1000))
+            secret_enc = SECRET.encode('utf-8')
+            string_to_sign = '{}\n{}'.format(timestamp, SECRET)
+            string_to_sign_enc = string_to_sign.encode('utf-8')
+            hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+            sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+            url = 'https://oapi.dingtalk.com/robot/send?access_token={}&timestamp={}&sign={}'.format(
+                TOKEN, timestamp, sign)
+            return url
+
+        url = get_url()
+        header = {
+            "Content-Type": "application/json",
+            "Charset": "UTF-8"
+        }
+        message = {
+            "msgtype": "text",
+            "text": {
+                "content": "{}@15626046299".format(msg)
+            },
+            "at": {
+                "atMobiles": [
+                    "15626046299",
+                ],
+                "isAtAll": False
+            }
+        }
+        message_json = json.dumps(message)
+        resp = requests.post(url=url, data=message_json, headers=header)
+        if resp.status_code == 200:
+            pass
+        else:
+            logger.warning("钉钉消息发送失败")
