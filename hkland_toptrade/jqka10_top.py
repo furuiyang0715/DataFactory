62868e912fe5843894621e2bec1a84db41c28176
furuiyang <furuiyang0715@gmail.com>
2020-06-10 15:34:58 +0800

    入库

--- hkland_toptrade/jqka10_top.py
+++ hkland_toptrade/jqka10_top.py
@@ -1,10 +1,27 @@
+import datetime
+import logging
+import os
+import sys
+import traceback
+
 import execjs
 import requests
 from lxml import html
 
-from hkland_toptrade.configs import JUY_HOST, JUY_PORT, JUY_USER, JUY_PASSWD, JUY_DB, PRODUCT_MYSQL_HOST, \
-    PRODUCT_MYSQL_PORT, PRODUCT_MYSQL_USER, PRODUCT_MYSQL_PASSWORD, PRODUCT_MYSQL_DB
+cur_path = os.path.split(os.path.realpath(__file__))[0]
+file_path = os.path.abspath(os.path.join(cur_path, ".."))
+sys.path.insert(0, file_path)
+
+from hkland_toptrade.configs import (JUY_HOST, JUY_PORT, JUY_USER, JUY_PASSWD, JUY_DB,
+                                     PRODUCT_MYSQL_HOST, PRODUCT_MYSQL_PORT, PRODUCT_MYSQL_USER,
+                                     PRODUCT_MYSQL_PASSWORD, PRODUCT_MYSQL_DB, LOCAL)
 from hkland_toptrade.sql_pool import PyMysqlPoolBase
+
+if LOCAL:
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+else:
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
 class JqkaTop10(object):
@@ -41,6 +58,8 @@ class JqkaTop10(object):
         self.juyuan_client = None
         self.product_client = None
         self.table_name = 'hkland_toptrade'
+        self.fields = ['Date', 'SecuCode', 'InnerCode', 'SecuAbbr', 'Close', 'ChangePercent',
+                       'TJME', 'TMRJE', 'TCJJE', 'CategoryCode']
 
     def _init_pool(self, cfg: dict):
         """
@@ -71,6 +90,69 @@ class JqkaTop10(object):
             self.juyuan_client.dispose()
         if self.product_client:
             self.product_client.dispose()
+
+    def contract_sql(self, datas, table: str, update_fields: list):
+        if not isinstance(datas, list):
+            datas = [datas, ]
+
+        to_insert = datas[0]
+        ks = []
+        vs = []
+        for k in to_insert:
+            ks.append(k)
+            vs.append(to_insert.get(k))
+        fields_str = "(" + ",".join(ks) + ")"
+        values_str = "(" + "%s," * (len(vs) - 1) + "%s" + ")"
+        base_sql = '''INSERT INTO `{}` '''.format(table) + fields_str + ''' values ''' + values_str
+
+        params = []
+        for data in datas:
+            vs = []
+            for k in ks:
+                vs.append(data.get(k))
+            params.append(vs)
+
+        if update_fields:
+            # https://stackoverflow.com/questions/12825232/python-execute-many-with-on-duplicate-key-update/12825529#12825529
+            # sql = 'insert into A (id, last_date, count) values(%s, %s, %s) on duplicate key update last_date=values(last_date),count=count+values(count)'
+            on_update_sql = ''' ON DUPLICATE KEY UPDATE '''
+            for update_field in update_fields:
+                on_update_sql += '{}=values({}),'.format(update_field, update_field)
+            on_update_sql = on_update_sql.rstrip(",")
+            sql = base_sql + on_update_sql + """;"""
+        else:
+            sql = base_sql + ";"
+        return sql, params
+
+    def _batch_save(self, sql_pool, to_inserts, table, update_fields):
+        try:
+            sql, values = self.contract_sql(to_inserts, table, update_fields)
+            count = sql_pool.insert_many(sql, values)
+        except:
+            traceback.print_exc()
+            logger.warning("失败")
+        else:
+            logger.info("批量插入的数量是{}".format(count))
+            sql_pool.end()
+            return count
+
+    def _save(self, sql_pool, to_insert, table, update_fields):
+        try:
+            insert_sql, values = self.contract_sql(to_insert, table, update_fields)
+            value = values[0]
+            count = sql_pool.insert(insert_sql, value)
+        except:
+            traceback.print_exc()
+            logger.warning("失败")
+        else:
+            if count == 1:
+                logger.info("插入新数据 {}".format(to_insert))
+            elif count == 2:
+                logger.info("刷新数据 {}".format(to_insert))
+            else:
+                logger.info("已有数据 {} ".format(to_insert))
+            sql_pool.end()
+            return count
 
     @property
     def cookies(self):
@@ -162,13 +244,13 @@ and ListedSector in (1, 2, 6, 7) and SecuCode = "{}";'.format(secu_code)
         }
         allitems = []
         for category, url in category_map.items():
-            # print(category, url)
             items = self.get_top(category, url)
             allitems.extend(items)
 
-        print(len(allitems))
-        for item in allitems:
-            print(item)
+        try:
+            self._batch_save(self.product_client, allitems, self.table_name, self.fields)
+        except:
+            pass
 
     def get_top(self, category, url):
         body = self.get(url)
@@ -233,15 +315,13 @@ and ListedSector in (1, 2, 6, 7) and SecuCode = "{}";'.format(secu_code)
                 item['CategoryCode'] = category
                 inner_code = self.get_juyuan_codeinfo(item['SecuCode'], category)
                 item['InnerCode'] = inner_code
+                item["Date"] = datetime.datetime.combine(datetime.datetime.today(), datetime.time.min)
+                item['CMFID'] = 1  # 兼容之前的程序 写死
+                item['CMFTime'] = datetime.datetime.now()  # 兼容和之前的程序 用当前的时间代替
                 items.append(item)
-
-        # for item in items:
-        #     print(item)
-
         return items
 
 
 if __name__ == "__main__":
     jqka = JqkaTop10()
-    # jqka.get_top()
     jqka.start()
