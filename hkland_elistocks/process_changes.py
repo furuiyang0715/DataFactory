f5a8e196406142aa6f870508f44c8c991b66887d
furuiyang <furuiyang0715@gmail.com>
2020-06-13 15:53:11 +0800

    Add some

---
+++ hkland_elistocks/process_changes.py
@@ -0,0 +1,41 @@
+# 将变更的记录进行归类
+
+change_addition = 'Addition'
+change_remove = ''
+
+addition_sentence = 'This stock will also be added to the List of Eligible SSE Securities for \
+Margin Trading and the List of Eligible SSE Securities for Short Selling'
+recover_sentence = 'This stock will also be added to the List of Eligible SSE Securities \
+for Margin Trading and the List of Eligible SSE Securities for Short Selling as it is \
+also included in SSE stock list for margin trading and shortselling.'
+remove_sentence = 'This stock will also be removed from the List of Eligible SSE Securities \
+for Margin Trading and the List of Eligible SSE Securities for Short Selling.'
+
+# 沪
+add_1 = [
+    '600070', '600984', '601512', '601816', '603053', '603068', '603218', '603489', '603520',
+    '603610', '603690', '603786', '603920', '603927', '603960'
+]
+
+add_134 = [
+    '600131', '600223', '600529', '600764', '601519', '603012', '603018', '603601', '603678',
+]
+
+recover_1 = [
+    '600988',
+]
+
+recover_134 = [
+    '600079', '600143', '600621', '600737', '600776', '600802', '603000',
+]
+
+remove_1 = [
+    '600693', '603007', '603080', '603165', '603332', '603339', '603351', '603603', '603773', '603877',
+    '603897', '603898',
+]
+
+remove_134 = [
+    '600123', '600230', '600231', '600239', '600297', '600398', '600418', '600499', '600528',
+    '600535', '600623', '600661', '600664', '600771', '600826', '600986', '601002', '601222',
+    '601997', '603959',
+]
