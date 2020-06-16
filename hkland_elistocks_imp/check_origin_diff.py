cf85b32e96144c60596d22333a86fdffcae3e7b5
furuiyang <furuiyang0715@gmail.com>
2020-06-16 17:54:57 +0800

    check

--- hkland_elistocks_imp/check_origin_diff.py
+++ hkland_elistocks_imp/check_origin_diff.py
@@ -1,20 +1,18 @@
 import datetime
 import functools
 import os
-import pickle
 import pprint
 import sys
-import time
 import traceback
 
 import schedule
-
-from hkland_elistocks_imp.configs import LOCAL
 
 cur_path = os.path.split(os.path.realpath(__file__))[0]
 file_path = os.path.abspath(os.path.join(cur_path, ".."))
 sys.path.insert(0, file_path)
 
+from hkland_elistocks_imp.const import records_sh, records_sz
+from hkland_elistocks_imp.configs import LOCAL
 from hkland_elistocks_imp.base import logger, BaseSpider
 
 
@@ -55,11 +53,85 @@ class OriginChecker(BaseSpider):
             ret = self.spider_client.select_all(sql)
         return ret
 
-    def select_onetime_records(self, dt):
-        pass
+    def select_onetime_records(self, table_name, onetime: datetime.datetime):
+        """获取某一个时间点的记录"""
+        self._test_init()
+        self._spider_init()
+        sql = '''select * from {} where Time = '{}'; '''.format(table_name, onetime)
+        if self.is_local:
+            ret = self.test_client.select_all(sql)
+        else:
+            ret = self.spider_client.select_all(sql)
+        return ret
 
-    def select_latest_records(self):
-        pass
+    def process_sz_changes(self, changes):
+        change_removal = 'Removal'
+        change_addition = 'Addition'
+        change_transfer = 'Transfer to List of Special SZSE Securities/Special China Connect Securities (stocks eligible for sell only)'
+        change_recover = 'Addition (from List of Special SZSE Securities/Special China Connect Securities (stocks eligible for sell only))'
+        addition_sentence = 'This stock will also be added to the List of Eligible SZSE Securities for Margin Trading and the List of Eligible SZSE Securities for Short Selling'
+        recover_sentence = 'This stock will also be added to the List of Eligible SZSE Securities for Margin Trading and the List of Eligible SZSE Securities for Short Selling as it is also included in SZSE stock list for margin trading and shortselling.'
+        remove_sentence = 'This stock will also be removed from the List of Eligible SZSE Securities for Margin Trading and the List of Eligible SZSE Securities for Short Selling.'
+
+        self.process("sz", changes, change_removal, change_addition, change_transfer, change_recover,
+                     addition_sentence, recover_sentence, remove_sentence)
+
+    def process_sh_changes(self, changes):
+        change_removal = 'Removal'
+        change_addition = 'Addition'
+        change_transfer = 'Transfer to List of Special SSE Securities/Special China Connect Securities (stocks eligible for sell only)'
+        change_recover = 'Addition (from List of Special SSE Securities/Special China Connect Securities (stocks eligible for sell only))'
+
+        addition_sentence = 'This stock will also be added to the List of Eligible SSE Securities for Margin Trading and the List of Eligible SSE Securities for Short Selling'
+        recover_sentence = 'This stock will also be added to the List of Eligible SSE Securities for Margin Trading and the List of Eligible SSE Securities for Short Selling as it is also included in SSE stock list for margin trading and shortselling.'
+        remove_sentence = 'This stock will also be removed from the List of Eligible SSE Securities for Margin Trading and the List of Eligible SSE Securities for Short Selling.'
+
+        self.process("sh", changes, change_removal, change_addition, change_transfer, change_recover,
+                     addition_sentence, recover_sentence, remove_sentence)
+
+    def process(self, flag, changes, change_removal, change_addition, change_transfer, change_recover,
+                addition_sentence, recover_sentence, remove_sentence):
+        add_134 = []
+        add_1 = []
+
+        recover_1 = []
+        recover_134 = []
+
+        transfer_1 = []
+        transfer_134 = []
+
+        removal_2 = []
+        for change in changes:
+            _change, _remarks, secu_code = change.get('Ch_ange'), change.get("Remarks"), change.get("SSESCode")
+            _effectivedate = change.get("EffectiveDate")
+            if _change == change_addition:
+                if addition_sentence in _remarks:
+                    add_134.append((secu_code, _effectivedate))
+                else:
+                    add_1.append((secu_code, _effectivedate))
+            elif _change == change_recover:
+                if recover_sentence in _remarks:
+                    recover_134.append((secu_code, _effectivedate))
+                else:
+                    recover_1.append((secu_code, _effectivedate))
+            elif _change == change_transfer:
+                if remove_sentence in _remarks:
+                    transfer_134.append((secu_code, _effectivedate))
+                else:
+                    transfer_1.append((secu_code, _effectivedate))
+            elif _change == change_removal:
+                removal_2.append((secu_code, _effectivedate))
+
+        print("{}_add_1: ".format(flag), add_1)
+        print("{}_add_134: ".format(flag), add_134)
+
+        print("{}_recover_1: ".format(flag), recover_1)
+        print("{}_recover_134: ".format(flag), recover_134)
+
+        print("{}_remove_1: ".format(flag), transfer_1)
+        print("{}_remove_134".format(flag), transfer_134)
+
+        print("{}_removal_2".format(flag), removal_2)
 
     def start(self):
         """直接检查两个数据点之间的差异"""
@@ -71,65 +143,60 @@ class OriginChecker(BaseSpider):
         origin_map = {
             'hkex_lgt_change_of_sse_securities_lists': '',
             'hkex_lgt_change_of_szse_securities_lists': '',
-
         }
-        # info = ''
-        # count = 1
+        info = ''
+        count = 1
         for table in origin_map:
             ret = self.get_distinct_spider_udpate_time(table)
             dt_list = sorted([r.get("Time") for r in ret])
             print("{} 至今全部的更新时间列表是{}".format(table, dt_list))
             # 注意: 最后的两次的差异 以及最后一次与第一次之间的差异 按需
-            latest_records = self.select_latest_records()
-            first_records = self.select_onetime_records(dt_list[0])
-            # first_records = ins.select_onetime_records(dt_list[-2])
-        #
-        #     # 去掉一些无关字段
-        #     for r in latest_records:
-        #         r.pop("id")
-        #         r.pop("Time")
-        #         r.pop("CREATETIMEJZ")
-        #         r.pop("ItemID")
-        #         r.pop("UPDATETIMEJZ")
-        #
-        #     for r in first_records:
-        #         r.pop("id")
-        #         r.pop("Time")
-        #         r.pop("CREATETIMEJZ")
-        #         r.pop("ItemID")
-        #         r.pop("UPDATETIMEJZ")
-        #
-        #     to_insert = []
-        #     to_delete = []
-        #     for one in latest_records:
-        #         if not one in first_records and not one in records_sh and not one in records_sz:
-        #             to_insert.append(one)
-        #
-        #     for one in first_records:
-        #         if not one in latest_records and not one in records_sh and not one in records_sz:
-        #             to_delete.append(one)
-        #
-        #     info += "{} 与第一相比 应该删除的记录是: {}\n".format(ins.table_name, len(to_delete))
-        #     info += "{} 与第一次相比, 应该增加的记录是: {}\n".format(ins.table_name, len(to_insert))
-        #
-        #     if count == 1:
-        #         process_sh_changes(to_insert)
-        #         pass
-        #     else:
-        #         process_sz_changes(to_insert)
-        #
-        #     with open("to_delete_{}.txt".format(count), "w") as f:
-        #         f.write(pprint.pformat(to_delete))
-        #     with open("to_insert_{}.txt".format(count), "w") as f:
-        #         f.write(pprint.pformat(to_insert))
-        #
-        #     ins.refresh_update_time()
-        #     count += 1
-        #
+            latest_records = self.select_onetime_records(table, dt_list[-1])
+            first_records = self.select_onetime_records(table, dt_list[0])
+
+            # 去掉一些无关字段
+            for r in latest_records:
+                r.pop("id")
+                r.pop("Time")
+                r.pop("CREATETIMEJZ")
+                r.pop("ItemID")
+                r.pop("UPDATETIMEJZ")
+
+            for r in first_records:
+                r.pop("id")
+                r.pop("Time")
+                r.pop("CREATETIMEJZ")
+                r.pop("ItemID")
+                r.pop("UPDATETIMEJZ")
+
+            to_insert = []
+            to_delete = []
+            for one in latest_records:
+                if not one in first_records and not one in records_sh and not one in records_sz:
+                    to_insert.append(one)
+
+            for one in first_records:
+                if not one in latest_records and not one in records_sh and not one in records_sz:
+                    to_delete.append(one)
+
+            info += "{} 与第一相比 应该删除的记录是: {}\n".format(table, len(to_delete))
+            info += "{} 与第一次相比, 应该增加的记录是: {}\n".format(table, len(to_insert))
+
+            if count == 1:
+                self.process_sh_changes(to_insert)
+            else:
+                self.process_sz_changes(to_insert)
+
+            with open("to_delete_{}.txt".format(count), "w") as f:
+                f.write(pprint.pformat(to_delete))
+            with open("to_insert_{}.txt".format(count), "w") as f:
+                f.write(pprint.pformat(to_insert))
+            count += 1
+
         # r1, r2 = list_check()
         # info += "沪股合资格校对的结果是 {}, 深股合资格校对的结果是 {}\n".format(r1, r2)
         # print(info)
-        # # tools.ding_msg(info)
+        # tools.ding_msg(info)
 
 
 def task():
---
+++ hkland_elistocks_imp/const.py
@@ -0,0 +1,61 @@
+import datetime
+
+records_sh = [
+    {'Ch_ange': 'Addition to List of Eligible SSE Securities for Margin Trading '
+    'and List of Eligible SSE Securities for Short Selling',
+    'EffectiveDate': datetime.date(2020, 4, 15),
+    'Remarks': 'Change is resulted from the change in SSE stock list for margin '
+    'trading and shortselling. For details, please refer to '
+    'http://www.sse.com.cn/lawandrules/sserules/main/trading/universal/c/c_20200410_5034247.shtml '
+    '(Chinese Version Only).',
+    'SSESCode': '601162',
+    'StockName': 'TIANFENG SECURITIES'},
+    {'Ch_ange': 'Addition to List of Eligible SSE Securities for Margin Trading '
+    'and List of Eligible SSE Securities for Short Selling',
+    'EffectiveDate': datetime.date(2020, 4, 15),
+    'Remarks': 'Change is resulted from the change in SSE stock list for margin '
+    'trading and shortselling. For details, please refer to '
+    'http://www.sse.com.cn/lawandrules/sserules/main/trading/universal/c/c_20200410_5034247.shtml '
+    '(Chinese Version Only).',
+    'SSESCode': '601236',
+    'StockName': 'HONGTA SECURITIES'},
+    {'Ch_ange': 'Addition to List of Eligible SSE Securities for Margin Trading '
+    'and List of Eligible SSE Securities for Short Selling',
+    'EffectiveDate': datetime.date(2020, 4, 15),
+    'Remarks': 'Change is resulted from the change in SSE stock list for margin '
+    'trading and shortselling. For details, please refer to '
+    'http://www.sse.com.cn/lawandrules/sserules/main/trading/universal/c/c_20200410_5034247.shtml '
+    '(Chinese Version Only).',
+    'SSESCode': '603501',
+    'StockName': 'WILL SEMICONDUCTOR'},
+    {'Ch_ange': 'Transfer to List of Special SSE Securities/Special China Connect '
+    'Securities (stocks eligible for sell only)',
+    'EffectiveDate': datetime.date(2020, 3, 31),
+    'Remarks': 'Change is resulted from the announcement issued by the listed '
+    'company on the Shanghai Stock Exchange website on 30 March 2020. '
+    'The stock is included in the Risk Alert Board. For details, '
+    'please refer to '
+    'http://static.sse.com.cn//disclosure/listedinfo/announcement/c/2020-03-28/600860_20200328_19.pdf '
+    '(Chinese Version Only).',
+    'SSESCode': '600860',
+    'StockName': 'BEIJING JINGCHENG MACHINERY ELEC (*ST)'}
+]
+
+records_sz = [
+    {'Ch_ange': 'Addition to List of Eligible SZSE Securities for Margin Trading '
+    'and List of Eligible SZSE Securities for Short Selling',
+    'EffectiveDate': datetime.date(2020, 4, 15),
+    'Remarks': 'Change is resulted from the change in SZSE stock list for margin '
+    'trading and shortselling. For details, please refer to '
+    'http://www.szse.cn/disclosure/margin/business/t20200410_575881.html '
+    '(Chinese Version Only)',
+    'SSESCode': '002463',
+    'StockName': 'WUS PRINTED CIRCUIT KUNSHAN'},
+    {'Ch_ange': 'Buy orders resumed',
+    'EffectiveDate': datetime.date(2020, 3, 18),
+    'Remarks': 'Change is resulted from the notification from SZSE that '
+    'Aggregate Foreign Shareholdings of the stock have gone below '
+    '26%. ',
+    'SSESCode': '000333',
+    'StockName': 'MIDEA GROUP'}
+]
