9d7a8469fb247e132e69e9703c5091096ab6b905
furuiyang <furuiyang0715@gmail.com>
2020-09-10 17:29:54 +0800

    update hkland_history

--- hkland_historytradestat/base_spider.py
+++ hkland_historytradestat/base_spider.py
@@ -1,9 +1,18 @@
+import base64
+import hashlib
+import hmac
+import json
 import logging
+import time
+import traceback
+import urllib.parse
 
-from hkland_historytradestat.configs import (SPIDER_MYSQL_USER, SPIDER_MYSQL_PORT, SPIDER_MYSQL_PASSWORD,
-                                             SPIDER_MYSQL_HOST, SPIDER_MYSQL_DB, PRODUCT_MYSQL_HOST, PRODUCT_MYSQL_PORT,
-                                             PRODUCT_MYSQL_USER, PRODUCT_MYSQL_PASSWORD, PRODUCT_MYSQL_DB, JUY_HOST,
-                                             JUY_PORT, JUY_USER, JUY_PASSWD, JUY_DB)
+import requests
+
+from hkland_historytradestat.configs import (PRODUCT_MYSQL_HOST, PRODUCT_MYSQL_PORT, PRODUCT_MYSQL_USER,
+                                             PRODUCT_MYSQL_PASSWORD, PRODUCT_MYSQL_DB, JUY_HOST, JUY_PORT,
+                                             JUY_USER, JUY_PASSWD, JUY_DB, SECRET, TOKEN, DC_HOST, DC_PORT, DC_USER,
+                                             DC_PASSWD, DC_DB)
 from hkland_historytradestat.sql_pool import PyMysqlPoolBase
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -11,13 +20,12 @@ logger = logging.getLogger(__name__)
 
 
 class BaseSpider(object):
-
-    spider_cfg = {
-        "host": SPIDER_MYSQL_HOST,
-        "port": SPIDER_MYSQL_PORT,
-        "user": SPIDER_MYSQL_USER,
-        "password": SPIDER_MYSQL_PASSWORD,
-        "db": SPIDER_MYSQL_DB,
+    dc_cfg = {
+        "host": DC_HOST,
+        "port": DC_PORT,
+        "user": DC_USER,
+        "password": DC_PASSWD,
+        "db": DC_DB,
     }
 
     product_cfg = {
@@ -36,6 +44,15 @@ class BaseSpider(object):
         "db": JUY_DB,
     }
 
+    def __init__(self):
+        self.fields = ["Date", 'MoneyInHistoryTotal', 'MarketTypeCode', 'MarketType',
+                       'MoneyIn', "MoneyBalance", "NetBuyAmount", "BuyAmount", "SellAmount"]
+        self.table_name = "hkland_historytradestat"
+        self.tool_table_name = 'base_table_updatetime'
+        self.juyuan_client = None
+        self.product_client = None
+        self.dc_client = None
+
     def _init_pool(self, cfg: dict):
         """
         eg.
@@ -52,7 +69,31 @@ class BaseSpider(object):
         pool = PyMysqlPoolBase(**cfg)
         return pool
 
-    def contract_sql(self, to_insert: dict, table: str, update_fields: list):
+    def _juyuan_init(self):
+        if not self.juyuan_client:
+            self.juyuan_client = self._init_pool(self.juyuan_cfg)
+
+    def _product_init(self):
+        if not self.product_client:
+            self.product_client = self._init_pool(self.product_cfg)
+
+    def _dc_init(self):
+        if not self.dc_client:
+            self.dc_client = self._init_pool(self.dc_cfg)
+
+    def __del__(self):
+        if self.juyuan_client:
+            self.juyuan_client.dispose()
+        if self.product_client:
+            self.product_client.dispose()
+        if self.dc_client:
+            self.dc_client.dispose()
+
+    def contract_sql(self, datas, table: str, update_fields: list):
+        if not isinstance(datas, list):
+            datas = [datas, ]
+
+        to_insert = datas[0]
         ks = []
         vs = []
         for k in to_insert:
@@ -61,12 +102,123 @@ class BaseSpider(object):
         fields_str = "(" + ",".join(ks) + ")"
         values_str = "(" + "%s," * (len(vs) - 1) + "%s" + ")"
         base_sql = '''INSERT INTO `{}` '''.format(table) + fields_str + ''' values ''' + values_str
-        on_update_sql = ''' ON DUPLICATE KEY UPDATE '''
-        update_vs = []
-        for update_field in update_fields:
-            on_update_sql += '{}=%s,'.format(update_field)
-            update_vs.append(to_insert.get(update_field))
-        on_update_sql = on_update_sql.rstrip(",")
-        sql = base_sql + on_update_sql + """;"""
-        vs.extend(update_vs)
-        return sql, tuple(vs)
+
+        params = []
+        for data in datas:
+            vs = []
+            for k in ks:
+                vs.append(data.get(k))
+            params.append(vs)
+
+        if update_fields:
+            # https://stackoverflow.com/questions/12825232/python-execute-many-with-on-duplicate-key-update/12825529#12825529
+            # sql = 'insert into A (id, last_date, count) values(%s, %s, %s) on duplicate key update last_date=values(last_date),count=count+values(count)'
+            on_update_sql = ''' ON DUPLICATE KEY UPDATE '''
+            for update_field in update_fields:
+                on_update_sql += '{}=values({}),'.format(update_field, update_field)
+            on_update_sql = on_update_sql.rstrip(",")
+            sql = base_sql + on_update_sql + """;"""
+        else:
+            sql = base_sql + ";"
+        return sql, params
+
+    def _batch_save(self, sql_pool, to_inserts, table, update_fields):
+        try:
+            sql, values = self.contract_sql(to_inserts, table, update_fields)
+            count = sql_pool.insert_many(sql, values)
+        except:
+            traceback.print_exc()
+            logger.warning("失败")
+        else:
+            logger.info("批量插入的数量是{}".format(count))
+            sql_pool.end()
+            return count
+
+    def _save(self, sql_pool, to_insert, table, update_fields):
+        try:
+            insert_sql, values = self.contract_sql(to_insert, table, update_fields)
+            value = values[0]
+            count = sql_pool.insert(insert_sql, value)
+        except:
+            traceback.print_exc()
+            logger.warning("失败")
+        else:
+            if count == 1:
+                logger.info("插入新数据 {}".format(to_insert))
+            elif count == 2:
+                logger.info("刷新数据 {}".format(to_insert))
+            else:
+                logger.info("已有数据 {} ".format(to_insert))
+            sql_pool.end()
+            return count
+
+    def ding(self, msg):
+        def get_url():
+            timestamp = str(round(time.time() * 1000))
+            secret_enc = SECRET.encode('utf-8')
+            string_to_sign = '{}\n{}'.format(timestamp, SECRET)
+            string_to_sign_enc = string_to_sign.encode('utf-8')
+            hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+            sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+            url = 'https://oapi.dingtalk.com/robot/send?access_token={}&timestamp={}&sign={}'.format(
+                TOKEN, timestamp, sign)
+            return url
+
+        url = get_url()
+        header = {
+            "Content-Type": "application/json",
+            "Charset": "UTF-8"
+        }
+        message = {
+            "msgtype": "text",
+            "text": {
+                "content": "{}@15626046299".format(msg)
+            },
+            "at": {
+                "atMobiles": [
+                    "15626046299",
+                ],
+                "isAtAll": False
+            }
+        }
+        message_json = json.dumps(message)
+        resp = requests.post(url=url, data=message_json, headers=header)
+        if resp.status_code == 200:
+            pass
+        else:
+            logger.warning("钉钉消息发送失败")
+
+    def refresh_update_time(self):
+        sql = '''select max(UPDATETIMEJZ) as max_dt from {}; '''.format(self.table_name)
+        max_dt = self.product_client.select_one(sql).get("max_dt")
+        logger.info("最新的更新时间是{}".format(max_dt))
+
+        refresh_sql = '''replace into {} (id,TableName, LastUpdateTime,IsValid) values (4, "hkland_historytradestat", '{}', 1); 
+        '''.format(self.tool_table_name, max_dt)
+        count = self.product_client.update(refresh_sql)
+        logger.info("1 首次插入 2 替换插入: {}".format(count))
+        self.product_client.end()
+
+    def _create_table(self):
+        sql = '''
+        CREATE TABLE IF NOT EXISTS `{}` (
+          `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+          `Date` datetime NOT NULL COMMENT '日期',
+          `MoneyIn` decimal(20,4) NOT NULL COMMENT '当日资金流入(百万）',
+          `MoneyBalance` decimal(20,4) NOT NULL COMMENT '当日余额（百万）',
+          `MoneyInHistoryTotal` decimal(20,4) NOT NULL COMMENT '历史资金累计流入(百万元）',
+          `NetBuyAmount` decimal(20,4) NOT NULL COMMENT '当日成交净买额(百万元）',
+          `BuyAmount` decimal(20,4) NOT NULL COMMENT '买入成交额(百万元）',
+          `SellAmount` decimal(20,4) NOT NULL COMMENT '卖出成交额(百万元）',
+          `MarketTypeCode` int(11) NOT NULL COMMENT '市场类型代码',
+          `MarketType` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '市场类型',
+          `CMFID` bigint(20) NOT NULL COMMENT '来源ID',
+          `CMFTime` datetime NOT NULL COMMENT '来源日期',
+          `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
+          `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+          PRIMARY KEY (`id`),
+          UNIQUE KEY `un` (`Date`,`MarketTypeCode`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='陆股通资金流向汇总(港股通币种为港元，陆股通币种为人民币)'; 
+        '''.format(self.table_name)
+        self.product_client.insert(sql)
+        self.product_client.end()
---
+++ hkland_historytradestat/jqka_history_spider.py
@@ -0,0 +1,327 @@
+import datetime
+import os
+import re
+import sys
+import execjs
+import requests
+from lxml import html
+
+
+cur_path = os.path.split(os.path.realpath(__file__))[0]
+file_path = os.path.abspath(os.path.join(cur_path, ".."))
+sys.path.insert(0, file_path)
+
+from hkland_historytradestat.base_spider import BaseSpider
+
+
+class JqkaHistory(BaseSpider):
+    def __init__(self):
+        super(JqkaHistory, self).__init__()
+        self.headers = {
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            # 'hexin-v': '',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+            'Accept': 'text/html, */*; q=0.01',
+            'Referer': 'http://data.10jqka.com.cn/hgt/ggtb/',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Connection': 'keep-alive',
+        }
+        self.market_map = {
+            "HG": (1, "沪股通"),
+            "SG": (3, "深股通"),
+            "GGh": (2, "港股通(沪市)"),
+            "GGs": (4, "港股通(深市)"),
+        }
+
+    def select_last_total(self, market_type, dt):
+        """查找距给出时间前一天的时间点的累计值"""
+        # sql = '''select Date, MoneyInHistoryTotal from hkland_historytradestat where Date = (select max(Date) \
+        # from hkland_historytradestat where MarketTypeCode = {}) and MarketTypeCode = {};'''.format(market_type, market_type)
+        sql = '''select Date, MoneyInHistoryTotal from hkland_historytradestat where Date = '{}' and MarketTypeCode = {};'''.format(dt, market_type)
+        ret = float(self.dc_client.select_one(sql).get("MoneyInHistoryTotal"))
+        return ret
+
+    @property
+    def cookies(self):
+        with open('jqka.js', 'r') as f:
+            jscont = f.read()
+        cont = execjs.compile(jscont)
+        cookie_v = cont.call('v')
+        cookies = {
+            'v': cookie_v,
+        }
+        return cookies
+
+    def get(self, url):
+        resp = requests.get(url, headers=self.headers, cookies=self.cookies)
+        if resp.status_code == 200:
+            return resp.text
+
+    @staticmethod
+    def re_decimal_data(data):
+        if isinstance(data, str):
+            data = float(data)
+        ret = float("%.4f" % data)
+        return ret
+
+    def re_str_data(self, data_str: str):
+        """将钱转为百万计"""
+        if data_str.endswith("万"):
+            data = self.re_decimal_data(data_str.replace("万", '')) / 10**2
+        elif data_str.endswith("亿"):
+            data = self.re_decimal_data(data_str.replace("亿", '')) * 10**2
+        else:
+            raise
+        return data
+
+    def start(self):
+        self._juyuan_init()
+        self._product_init()
+        self._dc_init()
+
+        self._create_table()
+
+        # 类别代码:GGh: 港股通(沪), GGs: 港股通(深), HG: 沪股通, SG: 深股通'
+        category_map = {
+            "HG": "http://data.10jqka.com.cn/hgt/hgtb/",
+            "GGh": "http://data.10jqka.com.cn/hgt/ggtb/",
+            "SG": "http://data.10jqka.com.cn/hgt/sgtb/",
+            "GGs": "http://data.10jqka.com.cn/hgt/ggtbs/",    # 无历史数据
+        }
+        allitems = []
+        for category, url in category_map.items():
+            items = self.get_history(category, url)
+            allitems.extend(items)
+
+        print("* " * 20)
+        for his in allitems:
+            print(his)
+
+        self._batch_save(self.product_client, allitems, self.table_name, self.fields)
+        self.refresh_update_time()
+
+    def get_history(self, category, url):
+        body = self.get(url)
+        doc = html.fromstring(body)
+        page_dts = doc.xpath(".//div[@class='table-tit']")
+        dt_infos = [page_dt.text_content().replace("\r\n", "") for page_dt in page_dts]
+        top_str = ''
+        for dt_info in dt_infos:
+            if "历史数据" in dt_info:
+                top_str = dt_info
+
+        if not top_str:
+            return []
+
+        print(top_str)
+        top_dt_str = re.findall("\d{4}-\d{2}-\d{2}", top_str)[0]
+        top_dt = datetime.datetime.strptime(top_dt_str, "%Y-%m-%d")
+        last_top_dt = top_dt - datetime.timedelta(days=1)
+        print("{} 的最近更新时间是 {}".format(category, top_dt))
+        '''
+        <table class="m-table J-ajax-table">
+            <thead>
+                <tr>
+                    <th width="100">日期</th>
+                    <th width="100" colidx="1">当日资金流入<br>(元)</th>
+                    <th width="100">当日余额<br>(元)</th>
+                    <th width="120">当日成交净买额<br>(元)</th>
+                    <th width="100">买入成交额<br>(元)</th>
+                    <th width="100">卖出成交额<br>(元)</th>
+                    <th width="110">领涨股</th>
+                    <th width="110">领涨股涨跌幅</th>
+                    <th width="100">上证指数</th>
+                    <th>涨跌幅</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tr class="">
+                    <td>2020-06-10</td>
+                    <td class="c-fall">-5.79亿</td>
+                    <td class="tc">525.79亿</td>
+                    <td class="c-fall">-12.33亿</td>
+                    <td class="">125.46亿</td>
+                    <td class="">137.79亿</td>
+                    <td><a href="http://stockpage.10jqka.com.cn/600859/" target="_blank">王府井</a></td>
+                    <td class="c-rise">10.01%</td>
+                    <td class="c-fall">2943.75</td>
+                    <td class="c-fall">-0.42%</td>
+            </tr>
+            <tr class="odd">
+                    <td>2020-06-09</td>
+                    <td class="c-rise">32.17亿</td>
+                    <td class="tc">487.83亿</td>
+                    <td class="c-rise">24.93亿</td>
+                    <td class="">149.53亿</td>
+                    <td class="">124.60亿</td>
+                    <td><a href="http://stockpage.10jqka.com.cn/600859/" target="_blank">王府井</a></td>
+                    <td class="c-rise">10.00%</td>
+                    <td class="c-rise">2956.11</td>
+                    <td class="c-rise">0.62%</td>
+            </tr>
+            <tr class="">
+                    <td>2020-06-08</td>
+                    <td class="c-rise">21.11亿</td>
+                    <td class="tc">498.89亿</td>
+                    <td class="c-rise">15.34亿</td>
+                    <td class="">163.50亿</td>
+                    <td class="">148.15亿</td>
+                    <td><a href="http://stockpage.10jqka.com.cn/600230/" target="_blank">沧州大化</a></td>
+                    <td class="c-rise">10.02%</td>
+                    <td class="c-rise">2937.77</td>
+                    <td class="c-rise">0.24%</td>
+            </tr>
+            <tr class="odd">
+                    <td>2020-06-05</td>
+                    <td class="c-rise">37.09亿</td>
+                    <td class="tc">482.91亿</td>
+                    <td class="c-rise">29.84亿</td>
+                    <td class="">141.37亿</td>
+                    <td class="">111.53亿</td>
+                    <td><a href="http://stockpage.10jqka.com.cn/600895/" target="_blank">张江高科</a></td>
+                    <td class="c-rise">10.02%</td>
+                    <td class="c-rise">2930.8</td>
+                    <td class="c-rise">0.40%</td>
+            </tr>
+            <tr class="">
+                    <td>2020-06-04</td>
+                    <td class="c-rise">10.84亿</td>
+                    <td class="tc">509.16亿</td>
+                    <td class="c-rise">4.86亿</td>
+                    <td class="">134.75亿</td>
+                    <td class="">129.88亿</td>
+                    <td><a href="http://stockpage.10jqka.com.cn/600565/" target="_blank">迪马股份</a></td>
+                    <td class="c-rise">10.13%</td>
+                    <td class="c-fall">2919.25</td>
+                    <td class="c-fall">-0.14%</td>
+            </tr>
+            <tr class="odd">
+                    <td>2020-06-03</td>
+                    <td class="c-rise">12.71亿</td>
+                    <td class="tc">507.29亿</td>
+                    <td class="c-rise">6.62亿</td>
+                    <td class="">162.59亿</td>
+                    <td class="">155.96亿</td>
+                    <td><a href="http://stockpage.10jqka.com.cn/600565/" target="_blank">迪马股份</a></td>
+                    <td class="c-rise">10.11%</td>
+                    <td class="c-rise">2923.37</td>
+                    <td class="c-rise">0.07%</td>
+            </tr>
+            <tr class="">
+                    <td>2020-06-02</td>
+                    <td class="c-rise">43.93亿</td>
+                    <td class="tc">476.07亿</td>
+                    <td class="c-rise">37.35亿</td>
+                    <td class="">179.92亿</td>
+                    <td class="">142.57亿</td>
+                    <td><a href="http://stockpage.10jqka.com.cn/600828/" target="_blank">茂业商业</a></td>
+                    <td class="c-rise">10.02%</td>
+                    <td class="c-rise">2921.4</td>
+                    <td class="c-rise">0.20%</td>
+            </tr>
+            <tr class="odd">
+                    <td>2020-06-01</td>
+                    <td class="c-rise">46.42亿</td>
+                    <td class="tc">473.58亿</td>
+                    <td class="c-rise">39.90亿</td>
+                    <td class="">188.95亿</td>
+                    <td class="">149.05亿</td>
+                    <td><a href="http://stockpage.10jqka.com.cn/600337/" target="_blank">美克家居</a></td>
+                    <td class="c-rise">10.11%</td>
+                    <td class="c-rise">2915.43</td>
+                    <td class="c-rise">2.21%</td>
+            </tr>
+            <tr class="">
+                    <td>2020-05-29</td>
+                    <td class="c-rise">20.80亿</td>
+                    <td class="tc">499.20亿</td>
+                    <td class="c-rise">10.82亿</td>
+                    <td class="">185.03亿</td>
+                    <td class="">174.22亿</td>
+                    <td><a href="http://stockpage.10jqka.com.cn/600185/" target="_blank">格力地产</a></td>
+                    <td class="c-rise">10.05%</td>
+                    <td class="c-rise">2852.35</td>
+                    <td class="c-rise">0.21%</td>
+            </tr>
+            <tr class="odd">
+                    <td>2020-05-28</td>
+                    <td class="c-rise">32.09亿</td>
+                    <td class="tc">487.91亿</td>
+                    <td class="c-rise">25.70亿</td>
+                    <td class="">158.43亿</td>
+                    <td class="">132.73亿</td>
+                    <td><a href="http://stockpage.10jqka.com.cn/600185/" target="_blank">格力地产</a></td>
+                    <td class="c-rise">10.07%</td>
+                    <td class="c-rise">2846.22</td>
+                    <td class="c-rise">0.33%</td>
+            </tr>
+            </tbody>
+            </table>
+        '''
+
+        history_table = doc.xpath(".//table[@class='m-table J-ajax-table']")[0]
+
+        # table_heads = history_table.xpath("./thead/tr/th")
+        # if table_heads:
+        #     table_heads = [table_head.text for table_head in table_heads]
+        #     print(table_heads)    # ['日期', '当日资金流入', '当日余额', '当日成交净买额', '买入成交额', '卖出成交额', '领涨股', '领涨股涨跌幅', '上证指数', '涨跌幅']
+        '''
+        `Date` datetime NOT NULL COMMENT '日期',
+        `MoneyIn` decimal(20,4) NOT NULL COMMENT '当日资金流入(百万）',
+        `MoneyBalance` decimal(20,4) NOT NULL COMMENT '当日余额（百万）',
+        `MoneyInHistoryTotal` decimal(20,4) NOT NULL COMMENT '历史资金累计流入(百万元）',
+        `NetBuyAmount` decimal(20,4) NOT NULL COMMENT '当日成交净买额(百万元）',
+        `BuyAmount` decimal(20,4) NOT NULL COMMENT '买入成交额(百万元）',
+        `SellAmount` decimal(20,4) NOT NULL COMMENT '卖出成交额(百万元）',
+        `MarketTypeCode` int(11) NOT NULL COMMENT '市场类型代码',
+        `MarketType` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '市场类型',
+        `CMFID` bigint(20) NOT NULL COMMENT '来源ID',
+        `CMFTime` datetime NOT NULL COMMENT '来源日期',
+        '''
+
+        table_heads = ["Date",           # 日期
+                       "MoneyIn",        # 当日资金流入(百万）
+                       "MoneyBalance",   # 当日余额
+                       "NetBuyAmount",   # 当日成交净买额(百万元）
+                       "BuyAmount",      # 买入成交额(百万元）
+                       "SellAmount",     # 卖出成交额(百万元）
+                       "headstock",      # 领涨股
+                       "headstockrise",  # 领涨股涨跌幅
+                       "index",          # 上证或者恒生指数
+                       "rate",           # 涨跌幅
+                       ]
+        unfields = ["headstock", "headstockrise", "index", "rate"]
+        moneyfields = ['MoneyIn', "MoneyBalance", "NetBuyAmount", "BuyAmount", "SellAmount"]
+        items = []
+        allhistory = history_table.xpath(".//tbody/tr")[:1]    # 只要最近一天的数据
+        # print(allhistory)
+        for history in allhistory:
+            trs = history.xpath("./td")
+            if trs:
+                top_info = [tr.text_content() for tr in trs]
+                item = dict(zip(table_heads, top_info))
+                for field in unfields:
+                    item.pop(field)
+
+                for field in moneyfields:
+                    item[field] = self.re_str_data(item.get(field))
+
+                # 历史资金累计流入(百万) = 上一天的历史资金累计流入(百万) + 今天的当日成交净买额(百万元)
+                last_money_in_history_total = self.select_last_total(self.market_map.get(category)[0], last_top_dt)
+                # print(">>>>>> ", last_money_in_history_total)
+                item['MoneyInHistoryTotal'] = last_money_in_history_total + item['NetBuyAmount']
+
+                item["Date"] = top_dt
+                _type_code, _type = self.market_map.get(category)
+                item['MarketTypeCode'] = _type_code
+                item['MarketType'] = _type
+                item['CMFID'] = 1  # 兼容之前的程序 写死
+                item['CMFTime'] = datetime.datetime.now()  # 兼容和之前的程序 用当前的时间代替
+                items.append(item)
+        return items
+
+
+if __name__ == "__main__":
+    jqka = JqkaHistory()
+    jqka.start()
