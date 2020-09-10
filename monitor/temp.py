737a4cb91229a25f30002851b062886f32050194
furuiyang <furuiyang0715@gmail.com>
2020-09-10 17:12:24 +0800

    rm monitor dir

--- monitor/temp.py
+++
@@ -1,9 +0,0 @@
-# https://www.tutorialspoint.com/python/time_strftime.htm
-
-import datetime
-
-
-_format = "%a, %d %b %Y %I:%M:%S %p %Z"
-_str = "Wed, 30 Apr 2008 00:00:00 GMT"
-ret = datetime.datetime.strptime(_str, _format)
-print(ret)
