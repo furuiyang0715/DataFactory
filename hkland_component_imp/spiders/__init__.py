b08405125a8e76d65eb33deabfd8bb1938bff739
furuiyang <furuiyang0715@gmail.com>
2020-09-14 15:12:14 +0800

    调整 readme 的显示

---
+++ hkland_component_imp/spiders/__init__.py
@@ -0,0 +1,17 @@
+'''
+self.sh_change_table_name = 'hkex_lgt_change_of_sse_securities_lists'
+self.sh_list_table_name = 'hkex_lgt_sse_securities'
+self.hk_change_table_name = 'lgt_sse_underlying_securities_adjustment'
+self.hk_list_table_name = 'hkex_lgt_sse_list_of_eligible_securities'
+'''
+
+
+class ComponentSpider(object):
+    """陆股通成分股爬虫表"""
+    def __init__(self):
+        self.web_url = ''''''
+
+
+
+
+    pass
