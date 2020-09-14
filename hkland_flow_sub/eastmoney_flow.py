26689f8d639330805c29c082f4e1a7df99ade75e
furuiyang <furuiyang0715@gmail.com>
2020-09-14 16:30:06 +0800

    adding

--- hkland_flow_sub/eastmoney_flow.py
+++ hkland_flow_sub/eastmoney_flow.py
@@ -1,6 +1,5 @@
 import datetime
 import json
-import logging
 import re
 import sys
 import time
@@ -15,21 +14,8 @@ from hkland_flow.configs import (SPIDER_MYSQL_HOST, SPIDER_MYSQL_PORT, SPIDER_MY
 from hkland_flow.sql_pool import PyMysqlPoolBase
 
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-
 class EMLGTNanBeiXiangZiJin(object):
     """陆股通实时数据(东财数据源) """
-
-    spider_cfg = {
-        "host": SPIDER_MYSQL_HOST,
-        "port": SPIDER_MYSQL_PORT,
-        "user": SPIDER_MYSQL_USER,
-        "password": SPIDER_MYSQL_PASSWORD,
-        "db": SPIDER_MYSQL_DB,
-    }
-
     def __init__(self):
         self.url = '''
         http://push2.eastmoney.com/api/qt/kamt.rtmin/get?fields1=f1,f2,f3,f4&fields2=f51,f52,f53,f54,f55,f56&ut=b2884a393a59ad64002292a3e90d46a5&cb=jQuery18306854619522421488_1566280636697&_=1566284477196'''
@@ -38,81 +24,6 @@ class EMLGTNanBeiXiangZiJin(object):
         self.table_name = 'hkland_flow_eastmoney'
         self.today = datetime.datetime.today().strftime("%Y-%m-%d")
         self.spider_client = None
-
-    def contract_sql(self, datas, table: str, update_fields: list):
-        if not isinstance(datas, list):
-            datas = [datas, ]
-
-        to_insert = datas[0]
-        ks = []
-        vs = []
-        for k in to_insert:
-            ks.append(k)
-            vs.append(to_insert.get(k))
-        fields_str = "(" + ",".join(ks) + ")"
-        values_str = "(" + "%s," * (len(vs) - 1) + "%s" + ")"
-        base_sql = '''INSERT INTO `{}` '''.format(table) + fields_str + ''' values ''' + values_str
-
-        params = []
-        for data in datas:
-            vs = []
-            for k in ks:
-                vs.append(data.get(k))
-            params.append(vs)
-
-        if update_fields:
-            # https://stackoverflow.com/questions/12825232/python-execute-many-with-on-duplicate-key-update/12825529#12825529
-            # sql = 'insert into A (id, last_date, count) values(%s, %s, %s) on duplicate key update last_date=values(last_date),count=count+values(count)'
-            on_update_sql = ''' ON DUPLICATE KEY UPDATE '''
-            for update_field in update_fields:
-                on_update_sql += '{}=values({}),'.format(update_field, update_field)
-            on_update_sql = on_update_sql.rstrip(",")
-            sql = base_sql + on_update_sql + """;"""
-        else:
-            sql = base_sql + ";"
-        return sql, params
-
-    def _save(self, sql_pool, to_insert, table, update_fields):
-        try:
-            insert_sql, values = self.contract_sql(to_insert, table, update_fields)
-            value = values[0]
-            count = sql_pool.insert(insert_sql, value)
-        except:
-            traceback.print_exc()
-            logger.warning("失败")
-        else:
-            if count == 1:
-                logger.info("插入新数据 {}".format(to_insert))
-            elif count == 2:
-                logger.info("刷新数据 {}".format(to_insert))
-            else:
-                logger.info("已有数据 {} ".format(to_insert))
-            sql_pool.end()
-            return count
-
-    def _init_pool(self, cfg: dict):
-        """
-        eg.
-        conf = {
-                "host": LOCAL_MYSQL_HOST,
-                "port": LOCAL_MYSQL_PORT,
-                "user": LOCAL_MYSQL_USER,
-                "password": LOCAL_MYSQL_PASSWORD,
-                "db": LOCAL_MYSQL_DB,
-        }
-        :param cfg:
-        :return:
-        """
-        pool = PyMysqlPoolBase(**cfg)
-        return pool
-
-    def spider_init(self):
-        if not self.spider_client:
-            self.spider_client = self._init_pool(self.spider_cfg)
-
-    def __del__(self):
-        if self.spider_client:
-            self.spider_client.dispose()
 
     def get_response_data(self):
         page = req.get(self.url).text
--- hkland_flow_sub/flow_base.py
+++ hkland_flow_sub/flow_base.py
@@ -1,6 +1,12 @@
+import logging
+import traceback
+
 from hkland_flow_sub.configs import (SPIDER_MYSQL_HOST, SPIDER_MYSQL_PORT, SPIDER_MYSQL_USER,
                                      SPIDER_MYSQL_PASSWORD, SPIDER_MYSQL_DB,
                                      )
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
 class FlowBase(object):
@@ -11,6 +17,81 @@ class FlowBase(object):
         "password": SPIDER_MYSQL_PASSWORD,
         "db": SPIDER_MYSQL_DB,
     }
+
+    def contract_sql(self, datas, table: str, update_fields: list):
+        if not isinstance(datas, list):
+            datas = [datas, ]
+
+        to_insert = datas[0]
+        ks = []
+        vs = []
+        for k in to_insert:
+            ks.append(k)
+            vs.append(to_insert.get(k))
+        fields_str = "(" + ",".join(ks) + ")"
+        values_str = "(" + "%s," * (len(vs) - 1) + "%s" + ")"
+        base_sql = '''INSERT INTO `{}` '''.format(table) + fields_str + ''' values ''' + values_str
+
+        params = []
+        for data in datas:
+            vs = []
+            for k in ks:
+                vs.append(data.get(k))
+            params.append(vs)
+
+        if update_fields:
+            # https://stackoverflow.com/questions/12825232/python-execute-many-with-on-duplicate-key-update/12825529#12825529
+            # sql = 'insert into A (id, last_date, count) values(%s, %s, %s) on duplicate key update last_date=values(last_date),count=count+values(count)'
+            on_update_sql = ''' ON DUPLICATE KEY UPDATE '''
+            for update_field in update_fields:
+                on_update_sql += '{}=values({}),'.format(update_field, update_field)
+            on_update_sql = on_update_sql.rstrip(",")
+            sql = base_sql + on_update_sql + """;"""
+        else:
+            sql = base_sql + ";"
+        return sql, params
+
+    def _save(self, sql_pool, to_insert, table, update_fields):
+        try:
+            insert_sql, values = self.contract_sql(to_insert, table, update_fields)
+            value = values[0]
+            count = sql_pool.insert(insert_sql, value)
+        except:
+            traceback.print_exc()
+            logger.warning("失败")
+        else:
+            if count == 1:
+                logger.info("插入新数据 {}".format(to_insert))
+            elif count == 2:
+                logger.info("刷新数据 {}".format(to_insert))
+            else:
+                logger.info("已有数据 {} ".format(to_insert))
+            sql_pool.end()
+            return count
+
+    def _init_pool(self, cfg: dict):
+        """
+        eg.
+        conf = {
+                "host": LOCAL_MYSQL_HOST,
+                "port": LOCAL_MYSQL_PORT,
+                "user": LOCAL_MYSQL_USER,
+                "password": LOCAL_MYSQL_PASSWORD,
+                "db": LOCAL_MYSQL_DB,
+        }
+        :param cfg:
+        :return:
+        """
+        pool = PyMysqlPoolBase(**cfg)
+        return pool
+
+    def spider_init(self):
+        if not self.spider_client:
+            self.spider_client = self._init_pool(self.spider_cfg)
+
+    def __del__(self):
+        if self.spider_client:
+            self.spider_client.dispose()
 
 
     pass
