ac007a6642ba49d50533dc4fe71713516f1257c6
furuiyang <furuiyang0715@gmail.com>
2020-09-14 18:27:26 +0800

    Little

--- hkland_flow_sub/flow_netin.py
+++ hkland_flow_sub/flow_netin.py
@@ -209,30 +209,30 @@ class EastMoneyFlowNetIn(FlowBase):
         for item in to_insert:
             self._save(self.spider_client, item, self.table_name, self.update_fields)
 
-    # def _create_table(self):
-    #     sql = '''
-    #      CREATE TABLE IF NOT EXISTS `{}` (
-    #       `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    #       `DateTime` datetime NOT NULL COMMENT '交易时间',
-    #       `ShHkNetBuyAmount` DECIMAL(19,4) COMMENT '沪股通/港股通(沪)净买额（万）',
-    #       `ShHkBuyAmount` DECIMAL(19,4) COMMENT '沪股通/港股通(沪) 买入额（万）',
-    #       `ShHkSellAmount` DECIMAL(19,4) COMMENT '沪股通/港股通(沪) 卖出额（万）',
-    #       `SzHkNetBuyAmount` DECIMAL(19,4) COMMENT '深股通/港股通(深)净买额（万）',
-    #       `SzHkBuyAmount` DECIMAL(19,4) COMMENT '深股通/港股通(深) 买入额（万）',
-    #       `SzHkSellAmount` DECIMAL(19,4) COMMENT '深股通/港股通(深) 卖出额（万）',
-    #       `TotalNetBuyAmount` DECIMAL(19,4) COMMENT '北向/南向净买额（万）',
-    #       `TotalBuyAmount` DECIMAL(19,4) COMMENT '北向/南向买入额（万）',
-    #       `TotalSellAmount` DECIMAL(19,4) COMMENT '北向/南向卖出额（万）',
-    #       `Category` tinyint(4) NOT NULL COMMENT '类别:1 南向, 2 北向',
-    #       `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
-    #       `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    #       PRIMARY KEY (`id`),
-    #       UNIQUE KEY `unique_key2` (`DateTime`,`Category`),
-    #       KEY `DateTime` (`DateTime`) USING BTREE
-    #     ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='陆港通-实时资金净买额-东财数据源';
-    #     '''.format(self.table_name)
-    #     self.spider_client.insert(sql)
-    #     self.spider_client.end()
+    def _create_table(self):
+        sql = '''
+         CREATE TABLE IF NOT EXISTS `{}` (
+          `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+          `DateTime` datetime NOT NULL COMMENT '交易时间',
+          `ShHkNetBuyAmount` DECIMAL(19,4) COMMENT '沪股通/港股通(沪)净买额（万）',
+          `ShHkBuyAmount` DECIMAL(19,4) COMMENT '沪股通/港股通(沪) 买入额（万）',
+          `ShHkSellAmount` DECIMAL(19,4) COMMENT '沪股通/港股通(沪) 卖出额（万）',
+          `SzHkNetBuyAmount` DECIMAL(19,4) COMMENT '深股通/港股通(深)净买额（万）',
+          `SzHkBuyAmount` DECIMAL(19,4) COMMENT '深股通/港股通(深) 买入额（万）',
+          `SzHkSellAmount` DECIMAL(19,4) COMMENT '深股通/港股通(深) 卖出额（万）',
+          `TotalNetBuyAmount` DECIMAL(19,4) COMMENT '北向/南向净买额（万）',
+          `TotalBuyAmount` DECIMAL(19,4) COMMENT '北向/南向买入额（万）',
+          `TotalSellAmount` DECIMAL(19,4) COMMENT '北向/南向卖出额（万）',
+          `Category` tinyint(4) NOT NULL COMMENT '类别:1 南向, 2 北向',
+          `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
+          `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+          PRIMARY KEY (`id`),
+          UNIQUE KEY `unique_key2` (`DateTime`,`Category`),
+          KEY `DateTime` (`DateTime`) USING BTREE
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='陆港通-实时资金净买额-东财数据源';
+        '''.format(self.table_name)
+        self.spider_client.insert(sql)
+        self.spider_client.end()
 
     def _start(self):
         # is_trading = self._check_if_trading_period()
