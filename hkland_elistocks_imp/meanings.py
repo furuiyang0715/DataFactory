da3026f7e2d6daefc8f3bc32eff6a7738bfa20b0
furuiyang <furuiyang0715@gmail.com>
2020-11-13 18:52:11 +0800

    增加变更释义

---
+++ hkland_elistocks_imp/meanings.py
@@ -0,0 +1,35 @@
+# change_removal_more = 'Remove from List of Eligible SZSE Securities for Margin Trading and List of Eligible SZSE Securities for Short Selling'
+# change_addition_less = 'Addition (from List of Special SZSE Securities/Special China Connect Securities (stocks eligible for sell only))'
+# change_transfer = 'Transfer to List of Special SZSE Securities/Special China Connect Securities (stocks eligible for sell only)'
+# change_recover = 'Addition (from List of Special SZSE Securities/Special China Connect Securities (stocks eligible for sell only))'
+# change_buyorders_resumed = 'Buy orders resumed'
+# change_buyorders_suspended = 'Buy orders suspended'
+# addition_sentence = 'This stock will also be added to the List of Eligible SZSE Securities for Margin Trading and the List of Eligible SZSE Securities for Short Selling'
+# recover_sentence = 'This stock will also be added to the List of Eligible SZSE Securities for Margin Trading and the List of Eligible SZSE Securities for Short Selling as it is also included in SZSE stock list for margin trading and shortselling.'
+# rename_sentence = 'SZSE Stock Code and Stock Name are changed'
+# print(change_addition_less == change_recover)
+
+
+# 标的类别(TargetCategory): 1-可买入及卖出，2-只可卖出，3-可进行保证金交易，4-可进行担保卖空，5-触发持股比例限制暂停买入。
+change_add1 = 'Addition'  # 該Ａ股將納入中華通證券
+
+# 加入可進行保證金交易的合資格上交所證券名單及可賣空的合資格滬股通證券名單
+change_add34 = 'Addition to List of Eligible SSE Securities for Margin Trading and List of Eligible SSE Securities for Short Selling'
+
+# 移至滬股通特別證券/中華通特別證券名單 (只可賣出)
+change_add2 = 'Transfer to List of Special SSE Securities/Special China Connect Securities (stocks eligible for sell only)'
+# 移入 2 时，一般要移出 1, 在有特别说明时移出  3 4
+sentence_rm34 = 'This stock will also be removed from the List of Eligible SZSE Securities for Margin Trading and the List of Eligible SZSE Securities for Short Selling.'
+
+# 移除, 从 2 中移除
+change_rm2 = 'Removal'
+
+# 加入(由滬股通特別證券/中華通特別證券名單 (只可賣出))
+# 意思为移出 2 加入 1
+change_addf2 = "Addition (from List of Special SSE Securities/Special China Connect Securities (stocks eligible for sell only))"
+
+# 已暫停買入
+change_suspended = 'Buy orders suspended'
+
+# 已恢復買入
+change_resumed = 'Buy orders resumed'
